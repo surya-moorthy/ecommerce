387fabadd75eac2278c2e2c29a8ab769c60bb0d5
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-04-20 12:07:17 +0530

    made me learn axios

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import { routes } from "./routes/routes";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/v1",routes);
 dotenv.config();
 
--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,12 +1,33 @@
-
+import axios from "axios";
 import './App.css'
+import { useState } from "react";
 
 function App() {
-  
-
+  const [username,setUsername] = useState("");
+  const [password,setpassword] = useState("");
+  const [email,setemail] = useState("");
   return (
-     <div>
-          Ecommerce website
+     <div className="flex flex-col gap-y-3 justify-center">
+          <div>
+             <label htmlFor='username'>Username:</label>
+             <input name="username" placeholder='enter your name:' onChange={(event)=>setUsername(event.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor='password'>Password:</label>
+            <input name="password" placeholder='enter your password:' onChange={(event)=>setpassword(event.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor='email'>Email:</label>
+            <input name="email" placeholder='enter your email:' onChange={(event)=>setemail(event.target.value)}/>
+          </div>
+          <button onClick={async ()=>{
+              const response = await axios.post("http://localhost:3000/api/v1/auth/register",{
+                username : username,
+                password : password,
+                email : email
+              })
+              console.log(response);
+          }} className="px-4 py-2 hover:bg-slate-400 w-32">Submit</button>
      </div>
   )
 }
